e1fdb5d923930547b80458bc4bad1a8984344bc7
gabrielnamur <gabriel.msilva@sptech.school>
2023-04-17 11:50:27 -0300

    Adicionei arquivos C++

---
+++ codigo-c-arduino-17.03.ino
@@ -0,0 +1,17 @@
+int ldr_pin = A0, leitura_ldr = 0;
+int contador = 1;
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  leitura_ldr = analogRead(ldr_pin);
+  Serial.print(contador);
+  Serial.print('.');
+  Serial.print(leitura_ldr);
+  Serial.println(";");
+  delay(2000);
+  contador++;
+}
+
+
